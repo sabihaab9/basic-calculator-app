dd49bdbd7156856c91792008ed5c737ddd694b8b
Sabiha Bary <sabihaab9@gmail.com>
2023-04-22 21:21:37 -0400

    added computation feature and All Clear button

--- script.js
+++ script.js
@@ -31,21 +31,68 @@ class Calculator{
         
         //add numbers to the screen  
         appendNumber(number){
+            //to only allow us to add one period 
+            //if we type the period key and we already have the period key,
+            //we just want to return, which will stop our function from executing from any further
+            if(number === '.' && this.currentOperand.includes('.')) return
+
             this.currentOperand = this.currentOperand.toString() + number.toString()
         }
 
 
         chooseOperation(operation){
-
+            if(this.currentOperand === '') return
+            if(this.previousOperand !== ''){
+                this.compute()
+            }
+            this.operation = operation
+            this.previousOperand = this.currentOperand
+            this.currentOperand = ""
         }
         
 
         compute(){
+            // create result "computation" variable
+            let computation  
+            //use parseFloat to convert string to decimal number
+            const prev = parseFloat(this.previousOperand)
+            const current = parseFload(this.currentOperand)
+
+            // we want to make sure that the code doesn't run if the user hits equal 
+            //when there is no prev or current operands.
+            if( isNaN(prev) || isNaN(current)) return 
+
+            //computation 
+            switch(this.operation){
+                case '+':
+                    computation = prev + current
+                    break
+
+                case '-':
+                    computation = prev - current
+                    break 
+               
+                case '*':
+                    computation = prev * current
+                    break 
+
+                case '÷':
+                    computation = prev / current
+                    break 
+
+                default:
+                    return
+            }
+            this.currentOperand = computation
+            this.previousOperand = ''
+            this.operation = undefined
+
 
         }
 
         updateDisplay(){
             this.currentOperandTextElement.innerText = this.currentOperand
+            this.previousOperandTextElement.innerText = this.previousOperand
 
         }
 }
@@ -67,7 +114,34 @@ numberButtons.forEach(button => {
 
         //add numbers on buttons to screen 
         calculator.appendNumber(button.innerText)
-        
+
         calculator.updateDisplay()
     })
 })
+
+operationButtons.forEach( button => {
+    button,addEventListener('click', () => {
+
+        //pass the text of the operation we are choosing
+        calculator.chooseOperation(button.innerText)
+        //update the display
+        calculator.updateDisplay()
+    })
+})
+
+
+equalsButton.addEventListener('click', button => {
+
+        //call compute function
+        calculator.compute()
+        //update the display
+        calculator.updateDisplay()
+    })
+
+allClearButton.addEventListener('click', button => {
+
+    //call clear function
+    calculator.clear()
+        //update the display
+    calculator.updateDisplay()
+    })
