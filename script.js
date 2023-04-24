a34c13d3beafccea71d872f056197170d9f5941e
Sabiha Bary <sabihaab9@gmail.com>
2023-04-23 22:06:38 -0400

    able to set values inside calculator

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//a class is a special function, blueprint for creating objects 
+//a class is a blueprint for creating objects 
 //inside a class, you define the properties and/or methods(functions)
 
 class Calculator{
@@ -19,6 +19,7 @@ class Calculator{
 
         //AC
         clear(){
+            //default values
             this.currentOperand = ''
             this.previousOperand = ''
             this.operation = undefined
@@ -26,70 +27,41 @@ class Calculator{
 
         //Delete
         delete(){
-            //take the current operand, convert to string & chop off the last character 
-            this.currentOperand = this.currentOperand.toString.slice(0,-1)
+            
         }
         
         //add numbers to the screen  
         appendNumber(number){
-            //to only allow us to add one period 
+            //to only allow us to add one period :
             //if we type the period key and we already have the period key,
             //we just want to return, which will stop our function from executing from any further
             if(number === '.' && this.currentOperand.includes('.')) return
 
+            //convert current opperand and number to strings so we can append them 
             this.currentOperand = this.currentOperand.toString() + number.toString()
         }
 
 
         chooseOperation(operation){
+            
             if(this.currentOperand === '') return
             if(this.previousOperand !== ''){
                 this.compute()
             }
             this.operation = operation
             this.previousOperand = this.currentOperand
-            this.currentOperand = ""
+            this.currentOperand = ''
         }
         
 
-        compute(){
-            // create result "computation" variable
-            let computation  
-            //use parseFloat to convert string to decimal number
-            const prev = parseFloat(this.previousOperand)
-            const current = parseFload(this.currentOperand)
+        compute(number){
 
-            // we want to make sure that the code doesn't run if the user hits equal 
-            //when there is no prev or current operands.
-            if( isNaN(prev) || isNaN(current)) return 
+            
 
-            //computation 
-            switch(this.operation){
-                case '+':
-                    computation = prev + current
-                    break
-
-                case '-':
-                    computation = prev - current
-                    break 
-               
-                case '*':
-                    computation = prev * current
-                    break 
-
-                case '÷':
-                    computation = prev / current
-                    break 
-
-                default:
-                    return
-            }
-            this.currentOperand = computation
-            this.previousOperand = ''
-            this.operation = undefined
-
-
+           
         }
+
+
 
         updateDisplay(){
             this.currentOperandTextElement.innerText = this.currentOperand
@@ -97,14 +69,16 @@ class Calculator{
 
         }
 }
+//constant variables that will be our buttons 
 
-const numberButtons = document.querySelectorAll("[data-number]")
+const numberButtons = document.querySelectorAll('[data-number]')
+console.log(numberButtons)
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
 const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
-const currentOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
 
 // create a calculator object
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
@@ -121,7 +95,7 @@ numberButtons.forEach(button => {
 })
 
 operationButtons.forEach( button => {
-    button,addEventListener('click', () => {
+    button.addEventListener('click', () => {
 
         //pass the text of the operation we are choosing
         calculator.chooseOperation(button.innerText)
@@ -131,26 +105,4 @@ operationButtons.forEach( button => {
 })
 
 
-equalsButton.addEventListener('click', button => {
 
-        //call compute function
-        calculator.compute()
-        //update the display
-        calculator.updateDisplay()
-    })
-
-allClearButton.addEventListener('click', button => {
-
-    //call clear function
-    calculator.clear()
-        //update the display
-    calculator.updateDisplay()
-})
-
-deleteButton.addEventListener('click', button => {
-
-    //call delete function
-    calculator.delete()
-        //update the display
-    calculator.updateDisplay()
-})
